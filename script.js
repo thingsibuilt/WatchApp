9c248aec649244a09fbf515c1748aecef8188686
thingsibuilt <92562575+thingsibuilt@users.noreply.github.com>
2023-09-10 20:47:31 -0500

    small update to above

--- script.js
+++ script.js
@@ -104,58 +104,40 @@ function showRingPanel() {
 // Function to populate bezel insert options with image previews
 async function populateBezelInserts() {
     try {
-     const data = [
-        {
-            "name": "Pepsi",
-            "imageUrl": "parts/bezel_inserts/pepsiInsert.png",
-            "partUrl": "https://luciusatelier.com/collections/bezel-inserts/products/skx007-ceramic-bezel-insert-slope-pepsi-dual-time-swiss-made"
-        },
-        {
-            "name": "Kermit",
-            "imageUrl": "parts/bezel_inserts/kermitInsert.png",
-            "partUrl": "https://luciusatelier.com/collections/bezel-inserts/products/skx013-aluminium-bezel-insert-slope-gmt-24h-sprite-black-green"
-        },
-        {
-            "name": "Yellow White",
-            "imageUrl": "parts/bezel_inserts/Yellow_White_Edit.png",
-            "partUrl": "https://www.aliexpress.us/item/3256804948452764.html?spm=a2g0o.productlist.main.25.3a1c51e555y8rs&algo_pvid=7ca4d68d-1cb5-4918-a435-71c9c661f5fb&algo_exp_id=7ca4d68d-1cb5-4918-a435-71c9c661f5fb-12&pdp_npi=4%40dis%21USD%2122.11%2113.71%21%21%21160.00%21%21%40210323f716938773528973523e7d7e%2112000031799795761%21sea%21US%212780216976%21A&curPageLogUid=XpgPQ76pzkTd"
-        }
-    ];
-    const bezelInsertOptions = document.getElementById('bezel-insert-options');
+        const response = await fetch('https://raw.githubusercontent.com/thingsibuilt/WatchApp/main/bezelInserts.json'); // Replace with the correct path to your JSON file
+        const data = await response.json();
+        const bezelInsertOptions = document.getElementById('bezel-insert-options');
 
-    data.forEach(item => {
-        // Create a container for each image, name, and cart icon
-        const container = document.createElement('div');
-        container.classList.add('part-container'); // Add a CSS class for styling if needed
+        data.forEach(item => {
+            // Create a container for each image, name, and link
+            const container = document.createElement('div');
 
-        // Create the image element
-        const insertImage = document.createElement('img');
-        insertImage.src = item.imageUrl;
-        insertImage.alt = item.name;
-        insertImage.classList.add('bezel-insert'); // Add a CSS class for styling if needed
+            // Create the image element
+            const insertImage = document.createElement('img');
+            insertImage.src = item.imageUrl;
+            insertImage.alt = item.name;
+            insertImage.classList.add('bezel-insert'); // Add a CSS class for styling if needed
 
-        // Create a paragraph element for the name text
-        const nameText = document.createElement('p');
-        nameText.textContent = item.name;
+            // Create a paragraph element for the name text
+            const nameText = document.createElement('p');
 
-        // Create a link for the part URL
-        const partLink = document.createElement('a');
-        partLink.href = item.partUrl;
-        partLink.target = '_blank'; // Open in a new tab
+            // Create a link for the part URL
+            const partLink = document.createElement('a');
+            partLink.href = item.partUrl;
+            partLink.textContent = item.name;
+            nameText.appendChild(partLink);
 
-        partLink.appendChild(nameText);
+            // Add a click event listener to update the main image
+            insertImage.addEventListener("click", function () {
+                document.getElementById("insert").src = insertImage.src;
+            });
 
-        // Add a click event listener to update the main image
-        insertImage.addEventListener("click", function () {
-            document.getElementById("main-image").src = insertImage.src;
-        });
+            // Append the image, name, and link to the container
+            container.appendChild(insertImage);
+            container.appendChild(nameText);
 
-        // Append the image and paragraph (with cart icon, name, and link) to the container
-        container.appendChild(insertImage);
-        container.appendChild(partLink);
-
-        // Append the container to the bezel insert options
-        bezelInsertOptions.appendChild(container);
+            // Append the container to the bezel insert options
+            bezelInsertOptions.appendChild(container);
         });
     } catch (error) {
         console.error('Error fetching or parsing JSON:', error);
